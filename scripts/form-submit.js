b7e45556bdf148abe6e438bcc00ac50c1e453896
Scalibra <scalibraas@Francins-MBP.home>
2021-10-16 00:24:26 +0200

    Initial commit, pushing up existing

---
+++ scripts/form-submit.js
@@ -0,0 +1,5 @@
+const homecontactform = document.querySelector("#home-contact-form");
+
+if(homecontactform){
+    
+}
---
+++ scripts/loadHeaderFooter.js
@@ -0,0 +1,7 @@
+$(function(){
+    $(document).ready(function(){
+        $(".navbar").load("modularity/header.html"); 
+        $(".site-footers").load("modularity/footer.html"); 
+    });
+});
+
